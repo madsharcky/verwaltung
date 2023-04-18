3d2fabbc81283dfc1e90559f0bcfad3f84a00114
MadSharcky <madsharcky@live.com>
2023-04-18 19:46:44 +0200

    Alle Klassen erstellt

--- src/main/java/ch/benedict/App.java
+++ src/main/java/ch/benedict/App.java
@@ -1,13 +1,7 @@
 package ch.benedict;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
     }
 }
---
+++ src/main/java/ch/benedict/HauptFenster.java
@@ -0,0 +1,5 @@
+package ch.benedict;
+
+public class HauptFenster {
+
+}
