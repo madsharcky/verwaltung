0153f093e0daa541b300edd0a6c6e9dd2cdc41dd
KkyaNite <raphaelschmid@bluewin.ch>
2023-05-11 21:15:06 +0200

    added classes Person and PersonenStorage

--- src/main/java/ch/benedict/Person.java
+++ src/main/java/ch/benedict/Person.java
@@ -1,5 +1,77 @@
 package ch.benedict;
 
 public class Person {
-
+    private String name;
+    private String vorname;
+    private String geschlecht;
+    private String geburtsdatum;
+    private String ahv;
+    private String region;
+    private int kinder;
+    
+    public Person(String name, String vorname, String geschlecht, String geburtsdatum, String ahv, String region, int kinder) {
+        this.name = name;
+        this.vorname = vorname;
+        this.geschlecht = geschlecht;
+        this.geburtsdatum = geburtsdatum;
+        this.ahv = ahv;
+        this.region = region;
+        this.kinder = kinder;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getVorname() {
+        return vorname;
+    }
+    
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    
+    public String getGeschlecht() {
+        return geschlecht;
+    }
+    
+    public void setGeschlecht(String geschlecht) {
+        this.geschlecht = geschlecht;
+    }
+    
+    public String getGeburtsdatum() {
+        return geburtsdatum;
+    }
+    
+    public void setGeburtsdatum(String geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+    
+    public String getAHV() {
+        return ahv;
+    }
+    
+    public void setAHV(String ahv) {
+        this.ahv = ahv;
+    }
+    
+    public String getRegion() {
+        return region;
+    }
+    
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    
+    public int getKinder() {
+        return kinder;
+    }
+    
+    public void setKinder(int kinder) {
+        this.kinder = kinder;
+    }
 }
--- src/main/java/ch/benedict/PersonenStorage.java
+++ src/main/java/ch/benedict/PersonenStorage.java
@@ -1,5 +1,47 @@
 package ch.benedict;
+import java.util.ArrayList;
 
 public class PersonenStorage {
+    private ArrayList<Person> personen;
+    private int position;
 
+    public PersonenStorage() {
+        this.personen = new ArrayList<>();
+        this.position = -1;  // position to -1, indicating that there is no current Person selected.
+    }
+
+    public Person getPerson() {
+        if (position >= 0 && position < personen.size()) {
+            return personen.get(position);
+        }
+        return null;
+    }
+
+    public boolean speichern() {
+        // implementation for saving personen to file or database
+        return true;
+    }
+
+    public boolean hinzufugen(String name, String vorname, String geschlecht, String ahv, String region, int kinder) {
+        Person p = new Person(name, vorname, geschlecht, null, ahv, region, kinder);
+        return personen.add(p);
+    }
+
+    public void loeschen() {
+        if (position >= 0 && position < personen.size()) {
+            personen.remove(position);
+        }
+    }
+
+    public void vorwaerts() {
+        if (position < personen.size() - 1) {
+            position++;
+        }
+    }
+
+    public void zurueck() {
+        if (position > 0) {
+            position--;
+        }
+    }
 }
